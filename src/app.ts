894e24e6674dbd57f5a3c4383239e9698408d5a3
Gilson Alencar <gilson.alencar94@gmail.com>
2022-05-13 17:56:01 -0300

    ✨ feat(routes): create GET:/products/ route
    route lists all products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import productsRouter from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (_req, res) => res.send('ok'));
+
+app.use('/products', productsRouter);
 
 export default app;
