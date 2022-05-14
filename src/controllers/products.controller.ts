fbbcd037f6844d30ba714b7f2dfceb42bf262bf3
Gilson Alencar <gilson.alencar94@gmail.com>
2022-05-14 19:45:38 -0300

    ✨ feat(products): add create controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
-import productsService from '../services/products.service';
+import * as productsService from '../services/products.service';
 
 async function getAll(_req: Request, res: Response) {
   const products = await productsService.getAll();
   res.status(200).json(products);
 }
 
-export default { getAll };
+async function create(req: Request, res: Response) {
+  const { name, amount } = req.body;
+  await productsService.create({ name, amount });
+  res.status(201);
+}
+
+export default {
+  getAll,
+  create,
+};
